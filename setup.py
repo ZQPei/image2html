06c1d1734593ec753964b92f3331a9959b14638d
ziqiang.pzq <ziqiang.pzq@alibaba-inc.com>
2020-07-20 18:10:34 +0800

    0.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='image2html',
-    version='0.0.2',
+    version='0.0.6',
     author='ZQPei',
     author_email='peiziqiang@gmail.com',
     packages=['image2html'],
@@ -18,8 +18,8 @@ setup(
           },
     url='https://github.com/ZQPei/image2html',
     description='Display images with html file.',
-    # long_description=open('./README.md', 'r').read(), 
-    # long_description_content_type="text/markdown",
+    long_description=open('./README.md', 'r').read(), 
+    long_description_content_type="text/markdown",
     platforms=["all"],
     license='MIT',
 )
